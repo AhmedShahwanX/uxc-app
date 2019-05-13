d41ac6106cc7ce21427dc649153c9d897c7d3365
Ahmed Shahwan <ahmed@shahwan.me>
2019-05-13 09:49:33 +0200

    views simple docs

--- catalog/views.py
+++ catalog/views.py
@@ -5,20 +5,48 @@ from .serializers import CategorySerializer, ShopSerializer, ProductSerializer, 
 
 
 class CategoryList(generics.ListCreateAPIView):
+    """
+    get:
+    Return a list of all the existing categories.
+
+    post:
+    Create a new category instance.
+    """
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
 class ShopList(generics.ListCreateAPIView):
+    """
+    get:
+    Return a list of all shops.
+
+    post:
+    Create a new shop.
+    """
     queryset = Shop.objects.all()
     serializer_class = ShopSerializer
 
 
 class ProductList(generics.ListCreateAPIView):
+    """
+    get:
+    Return a list of all products.
+
+    post:
+    Create a new product.
+    """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class ProductImages(generics.ListCreateAPIView):
+    """
+    get:
+    Return a list of all products' images.
+
+    post:
+    Create a new product image.
+    """
     queryset = ProductImage.objects.all()
     serializer_class = ProductImageSerializer
